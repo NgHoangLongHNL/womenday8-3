73c0306b37747adbe8faae495c7e7079fe59d196
Mr.Low <nguyenlonghnl09@gmail.com>
2025-03-08 22:32:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Kính gửi các thằng đàn bà!',
+    introDesc: `Nhân ngày Quốc tế Phụ nữ, xin gửi những lời chúc 8/3 nồng nhiệt nhất đến toàn thể các bạn nữ. Chúc các bạn ngày một xinh đẹp để ngày càng có thêm nhiều chàng trai đeo bám nhằng nhằng như lửa bám xăng, như răng bám lợi, như trời bám mây, như cây bám đất, như bít tất bám... bàn chân!`,
+    btnIntro: 'Next',
+    title: 'Nhân ngày 8/3 quốc tế phụ nữ em có món quà dành tặng các chị 😙',
+    desc: 'Mời các chị chọn quà =)))',
+    btnYes: 'Không lấy quà =)))',
+    btnNo: 'Ai phỏn 1 tỷ pờ rào súp bờ mắc :3',
+    question:'Tại sao lại không lấy Ai phỏn 1 tỷ pờ rào súp bờ mắc <3',
+    btnReply: 'Seen <3',
+    reply: 'Tại vì chị chỉ cần em thôi <33333333',
+    mess: 'a lêu lêu có người bị troll hahahahahahahah',
+    messDesc: 'Copyright: NgHoangLong',
+    btnAccept: 'Facebook me <3',
+    messLink: 'http://www.facebook.com/hnl04dev'
+}
